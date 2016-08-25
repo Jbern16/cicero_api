47f9b544b80570b0db5ee143f9ca296dcbdf7eb0
Jbern16 <jbern16@gmail.com>
2016-08-25 14:28:42 -0400

    rough implementation of querying for streaming platforms

--- app/services/guide_box_service.rb
+++ app/services/guide_box_service.rb
@@ -4,14 +4,28 @@ class GuideBoxService
     @connection = Faraday.new(url: "http://api-public.guidebox.com") 
   end  
 
-  def get_movie
-    encoded_name = triple_encode("Stranger Than Fiction")
+  def get_movie(name, search_type="exact")
     response = @connection.get do |req|
-      req.url "/v1.43/US/#{ENV['guide_box_key']}/search/movie/title/#{encoded_name}/fuzzy"
+      encoded_name = triple_encode(name)
+      req.url "/v1.43/US/#{ENV['guide_box_key']}/search/movie/title/#{encoded_name}/#{search_type}"
     end 
-    binding.pry
+  end
+
+  def get_full_movie_data(movie)
+    response = @connection.get do |req|
+      req.url "/v1.43/US/#{ENV['guide_box_key']}/movie/#{movie.id}"
+    end
+  end
+
+  def get_streaming_data(movie)
+    movie_data = parse_response(get_full_movie_data(movie)
+    free_sources = movie_data.free_web_sources
+    paid_sources = movie_data.purchase_web_sources
   end 
 
+  def parse_response(response)
+    JSON.parse(response.body)
+  end 
 
   private 
     def triple_encode(name)
@@ -19,5 +33,4 @@ class GuideBoxService
         CGI.escape(name)
       end 
     end 
-
 end 
