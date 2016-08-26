1dc72e3c912b299fcc6581104eeb569bfe0c6b3c
Jbern16 <jbern16@gmail.com>
2016-08-26 14:11:50 -0400

    adds ability to get streaming data

--- app/services/guide_box_service.rb
+++ app/services/guide_box_service.rb
@@ -13,19 +13,25 @@ class GuideBoxService
 
   def get_full_movie_data(movie)
     response = @connection.get do |req|
-      req.url "/v1.43/US/#{ENV['guide_box_key']}/movie/#{movie.id}"
+      req.url "/v1.43/US/#{ENV['guide_box_key']}/movie/#{movie[:id]}"
     end
   end
 
   def get_streaming_data(movie)
-    movie.merge(construct_sources(parse_response(get_full_movie_data(movie))
+    movie = parse_response(get_full_movie_data(movie))
+    construct_sources(movie)  
+  end 
+
+  def parse_search_response(response)
+    parse_response(response)[:results]
   end 
 
   def parse_response(response)
-    JSON.parse(response.body)
-  end 
+    JSON.parse(response.body, symbolize_names: true)
+  end
 
   private 
+  
     def triple_encode(name)
       3.times do 
         CGI.escape(name)
@@ -33,7 +39,9 @@ class GuideBoxService
     end
 
     def construct_sources(movie_data)
-      Hash.new(free_sources: movie_data.free_sources, 
-               paid_sources: movie_data.paid_sources)
+      {
+        free_sources: movie_data[:free_web_sources], 
+        paid_sources: movie_data[:purchase_web_sources]
+      }
     end  
 end 
--- spec/services/guide_box_service_spec.rb
+++ spec/services/guide_box_service_spec.rb
@@ -2,9 +2,17 @@ require 'rails_helper'
 
 describe "can retrive movie info from guidebox api" do 
   it 'has proper attributes' do 
-    service = GuideBoxService.new 
-    response   = service.get_movie
+    service    = GuideBoxService.new 
+    response   = service.get_movie("Stranger Than Fiction")
 
     expect(response).to eq ""
-  end 
+  end
+
+  it "can get streaming data from id" do 
+    service  = GuideBoxService.new
+    movies = service.parse_search_response(service.get_movie("Stranger Than Fiction"))
+    streaming_data = service.get_streaming_data(movies.first)
+    
+    expect(streaming_data).to eq ""
+  end  
 end 
